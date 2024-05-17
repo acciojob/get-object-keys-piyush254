ffbb0e4db421aa154b4bc19efadf1d95e1950242
piyush254 <76161817+piyush254@users.noreply.github.com>
2024-05-17 16:39:35 +0530

    Solution

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let student = {
+  name: "Dhoni",
+};
+
+student.__proto__.getKeys = function () {
+  return Object.keys(this);
+}
+
+
+console.log(student.getKeys())
